901d623033e19042aadd869d86777fdf3c8f28ba
Sythe Veenje <sythe@burobaum.nl>
2016-02-23 13:39:31 +0100

    naked theme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # NakedPress
+
 Very basic Wordpress theme to start with
+
+You'll find a "your_theme_name" folder in the wp-content/themes/ directory. Rename this folder and the themename in wp-content/themes/your_theme_name/style.css before installing the theme.
+
+Have fun!
---
+++ wp-content/themes/your_theme_name/index.php
@@ -0,0 +1,5 @@
+<?php TemplateParts::get_template_parts( array( 'template-parts/shared/html-header', 'template-parts/shared/header' ) ); ?>
+
+<!-- YOUR CONTENT HERE; Create a Loop or load custom code -->
+
+<?php TemplateParts::get_template_parts( array( 'template-parts/shared/footer','template-parts/shared/html-footer' ) ); ?>
